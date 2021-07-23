e02033d9ea7a31cf1ac8ae4a683a85bd05cba213
phou0215 <phou0215@gmail.com>
2021-07-23 10:50:16 +0900

    2021-07-23 cmds ADB wireless 기능 추가 commit

---
+++ adbController/exe_g660.py
@@ -0,0 +1,69 @@
+import os
+import sys
+import re
+import subprocess
+import re
+import time
+from time import sleep
+from datetime import datetime
+from cmds import CMDS
+
+class SAMPLE_Test():
+
+    def __init__(self, uuid, divide_window=10):
+
+        super().__init__()
+        self.cmd = CMDS(uuid, divide_window)
+        # self.display_flag = False
+        # self.end_flag = False
+
+
+    def run_script(self):
+
+        # 필수 setup method 반드시 호출
+        self.cmd.setup_test()
+        # ########################__비행기 모드 테스트__########################
+        # self.cmd.cmd_status_airplaneOnOff(exe_type=1, delay=1)
+        # self.cmd.cmd_status_backButton(iter_count=2)
+        # sleep(2)
+        # self.cmd.cmd_status_airplaneOnOff(exe_type=0, delay=1)
+        # self.cmd.cmd_status_backButton(iter_count=2)
+        # sleep(2)
+        # self.cmd.cmd_status_airplaneOnOff(exe_type=0,delay=1)
+        # self.cmd.cmd_status_backButton(iter_count=2)
+        #
+        # self.cmd.cmd_status_autoRotateOnOff(exe_type=1, delay=2)
+        # sleep(3)
+        # self.cmd.cmd_status_autoRotateOnOff(exe_type=0, delay=2)
+        # sleep(3)
+        # self.cmd.cmd_status_backButton(iter_count=2)
+        for i in range(3):
+            # ########################__BlueTooth 모드 테스트__########################
+            self.cmd.cmd_status_blueToothOnOff(exe_type=1, delay=1)
+            self.cmd.cmd_status_backButton(iter_count=2)
+            sleep(2)
+            self.cmd.cmd_status_blueToothOnOff(exe_type=0, delay=1)
+            self.cmd.cmd_status_backButton(iter_count=2)
+            sleep(2)
+            self.cmd.cmd_status_blueToothOnOff(exe_type=0, delay=1)
+            self.cmd.cmd_status_backButton(iter_count=2)
+
+            # # ########################__화면 회전 모드 테스트__########################
+            self.cmd.cmd_status_autoRotateOnOff(exe_type=1, delay=2)
+            sleep(3)
+            self.cmd.cmd_status_autoRotateOnOff(exe_type=0, delay=2)
+            sleep(3)
+            self.cmd.cmd_status_backButton(iter_count=2)
+            self.cmd.cmd_status_screenShot(delay=2, name='sample_test1')
+
+
+if __name__ == "__main__":
+
+    g960 = SAMPLE_Test('1c25c664460c7ece', divide_window=20)
+    # lm350 = SAMPLE_Test('LMV350N7a33ed5c', divide_window=20)
+    g960.run_script()
+    # lm350.run_script()
+
+
+
+
